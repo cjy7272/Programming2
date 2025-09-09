1e50123c2c5a22dfc74ad6b8a2f3bff9ba266c50
ldj202111001 <seon3129@gmail.com>
2025-09-09 13:02:24 +0900

    .

---
+++ Myfrist/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World!");
+
+	return 0;
+}
+
+void test1()
+{
+	int age = 20;
+	const double PI = 3.141592;
+	char myname[7] = "ÃÖÁÖ¿µ";
+	int iarray[] = { 1, 2, 3, 4 };
+
+	age = age + 1;
+
+	double r = 2;
+	double area = PI * r * r;
+	
+	if ((age > 10) || (area > 5.0))
+	{
+
+	}
+	else 
+	{
+
+	}
+	
+
+}
